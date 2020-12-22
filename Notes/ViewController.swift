526535a42b36152a5cf9fc68891bcbd838226788
Marcelo Ludovico de Paula <marcelo.paula@eptv.com.br>
2020-12-22 17:59:38 -0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by marcelo on 22/12/2020.
+//  Copyright © 2020 marcelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
